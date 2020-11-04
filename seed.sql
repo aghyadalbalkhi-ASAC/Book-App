f3b6432399eb763b3b286a685bbc07df67e106b5
Dianashafee <diana.shafee1997@hotmail.com>
2020-11-04 17:53:06 +0200

    css changes

--- seed.sql
+++ seed.sql
@@ -9,4 +9,4 @@ CREATE TABLE books (
 
      INSERT INTO books (title, Author, isbn, image_url, descr) VALUES ('the old man and the sea', 'Ernest','ISBN_13 9780441013593','http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_ap', 'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny');
 
-     INSERT INTO books (title, Author, isbn, image_url, descr) VALUES ('A&D in Coding301', 'Aghiad','ISBN_13 9780441013456','https://i.imgur.com/J5LVHEL.jpg', 'andventures in Cooding');
+     INSERT INTO books (title, Author, isbn, image_url, descr) VALUES ('A&D in Coding301', 'Aghiad','ISBN_13 9780441013456','https://books.google.com/books/content?id=vHRVDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'andventures in Cooding');
